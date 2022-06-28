ff627266c165a7417be25a47a1bd0627b3a0eea9
Emmanuel Ugwu <>
2022-06-28 17:13:11 +0100

    made changes

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <p>display post of ID {{ $route.params.id }}</p>
+    <button>
+      <router-link to="/posts"> go back to posts</router-link>
+    </button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+p {
+  text-transform: uppercase;
+}
+button {
+  background: hsla(160, 100%, 37%, 1);
+  border: transparent;
+  padding: 0.5rem;
+  border-radius: 25px;
+  margin-top: 0.3rem;
+}
+
+a {
+  color: white;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="posts">
+    <h1>posts</h1>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/1">Post 1</router-link>
+      </li>
+      <li>
+        <router-link to="/postDetail/2">Post 2</router-link>
+      </li>
+      <li>
+        <router-link to="/postDetail/3">Post 3</router-link>
+      </li>
+    </ul>
+    <textarea v-autofocus cols="30" rows="10"></textarea>
+  </div>
+</template>
+
+<script setup>
+import { vAutofocus } from "../directives/vAutofocus";
+</script>
+
+<style scoped>
+.posts {
+  text-transform: uppercase;
+}
+
+ul {
+  list-style-type: none;
+}
+
+li {
+  margin: 0.5rem;
+}
+</style>
